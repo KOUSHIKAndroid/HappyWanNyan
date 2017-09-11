7a9e56dc603c8b23d1400648693ea9c87c314811
surajcse116 <suraj.shaw@esolzmail.com>
2017-09-11 14:37:44 +0530

    11 sep

--- app/src/main/java/com/happywannyan/SitterBooking/NewCardAddActivity.java
+++ app/src/main/java/com/happywannyan/SitterBooking/NewCardAddActivity.java
@@ -2,7 +2,11 @@ package com.happywannyan.SitterBooking;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.happywannyan.Font.SFNFTextView;
 import com.happywannyan.R;
 
 /**
@@ -14,5 +18,54 @@ public class NewCardAddActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_card);
+
+
+
+        if(((EditText)findViewById(R.id.edtxt_card_name)).getText().toString().trim().equals(""))
+        {
+            ((TextInputLayout)findViewById(R.id.input_txt_card_name)).setErrorEnabled(true);
+            ((TextInputLayout)findViewById(R.id.input_txt_card_name)).setError("Please Enter card holder name");
+        }else {
+
+            ((TextInputLayout)findViewById(R.id.input_txt_card_name)).setErrorEnabled(false);
+            if(((EditText)findViewById(R.id.edtxt_card_number)).getText().toString().trim().equals("")){
+                ((TextInputLayout)findViewById(R.id.input_txt_card_number)).setErrorEnabled(true);
+                ((TextInputLayout)findViewById(R.id.input_txt_card_number)).setError("Please Enter card holder number");
+            }
+            else if(((EditText)findViewById(R.id.edtxt_card_number)).getText().toString().trim().length()<14){
+                ((TextInputLayout)findViewById(R.id.input_txt_card_number)).setErrorEnabled(true);
+                ((TextInputLayout)findViewById(R.id.input_txt_card_number)).setError("Please Enter correct card number");
+            }
+            else {
+                ((TextInputLayout)findViewById(R.id.input_txt_card_number)).setErrorEnabled(false);
+                if(((SFNFTextView)findViewById(R.id.tv_month)).getText().toString().trim().equals("00")){
+                    Toast.makeText(getApplicationContext(),"Please choose month",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if(((SFNFTextView)findViewById(R.id.tv_year)).getText().toString().trim().equals("00")){
+                        Toast.makeText(getApplicationContext(),"Please choose year",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+
+                        if(((EditText)findViewById(R.id.edt_cvv)).getText().toString().trim().equals("")){
+                            ((TextInputLayout)findViewById(R.id.input_cvv)).setErrorEnabled(true);
+                            ((TextInputLayout)findViewById(R.id.input_cvv)).setError("Please enter CVV");
+                        }
+                        else {
+                            ((TextInputLayout)findViewById(R.id.input_cvv)).setErrorEnabled(false);
+
+                            if(((EditText)findViewById(R.id.edt_cvv)).getText().toString().trim().length()<3){
+                                ((TextInputLayout)findViewById(R.id.input_cvv)).setErrorEnabled(true);
+                                ((TextInputLayout)findViewById(R.id.input_cvv)).setError("Please enter correct CVV");
+                            }
+                            else {
+                                ((TextInputLayout)findViewById(R.id.input_cvv)).setErrorEnabled(false);
+                                ///////////////submit task////////////////////////////////////
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
