ccbc64685386734f6a888268755ee02955ba6b54
surajcse116 <suraj.shaw@esolzmail.com>
2017-09-15 16:41:53 +0530

    15 sep

--- app/src/main/java/com/happywannyan/Fragments/MyPetsFragments.java
+++ app/src/main/java/com/happywannyan/Fragments/MyPetsFragments.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.happywannyan.Activities.AddAnotherPetsActivity;
 import com.happywannyan.Activities.BaseActivity;
 import com.happywannyan.Adapter.YourPets_Adapter;
