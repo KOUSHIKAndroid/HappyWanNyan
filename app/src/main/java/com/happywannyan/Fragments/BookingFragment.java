b418356b67f6ccd2ec52d5897291ac352b600a9d
surajcse116 <suraj.shaw@esolzmail.com>
2017-10-03 12:21:55 +0530

    3 oct by suraj

--- app/src/main/java/com/happywannyan/Fragments/BookingFragment.java
+++ app/src/main/java/com/happywannyan/Fragments/BookingFragment.java
@@ -226,7 +226,7 @@ public class BookingFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 222) {
-            AllBooking.clear();
+            //AllBooking.clear();
             loadList("0");
         }
     }
